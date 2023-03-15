0d395449457a0c6106839659da7c1c5eeb49f24b
AlexZhebet <115915899+AlexZhebet@users.noreply.github.com>
2023-03-15 13:04:31 +0200

    Update js.js

--- js.js
+++ js.js
@@ -1,17 +1,23 @@
-const button = document.querySelectorAll(`.button`),
- div = document.createElement(`div`);
+const addBtn = document.getElementById('buttonAdd')
+const delete2 = document.getElementById('delete')
 
-button[0].addEventListener(`click`, () => {
-    button[0].after(div);
-    div.classList.add(`button_text`);
-    div.innerHTML = `<p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Odio atque suscipit error esse nostrum neque aperiam
-    pariatur, sunt itaque in ut harum eius dolore deserunt et ipsa modi, numquam perferendis.</p>`;
-    div.addEventListener(`click`, () => {
-        div.classList.add(`purple`);
+addBtn.addEventListener('click', () => {
+    const bloc = toAdd()
+    bloc ? contentId.appendChild(bloc) : null
+})
+
+delete2.addEventListener('click', () => {
+    contentId.removeChild(contentId.children[contentId.children.length - 1])
+})
+
+function toAdd() {
+    if (!input.value) return null
+    const bloc = document.createElement('div')
+    bloc.classList.add('button_text')
+    bloc.innerText = input.value
+    input.value = ''
+    bloc.addEventListener('click', (e) => {
+        e.target.classList.toggle('purple')
     })
-});
-
-button[1].addEventListener(`click`, () => {
-    div.remove();
-    div.classList.remove(`purple`);
-}); 
+    return bloc
+}
